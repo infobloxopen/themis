9186117b4d22264bd0be929b1ac19516441f8a94
Vasili Vasilyeu <vasili.v@tut.by>
2017-01-31 16:58:27 +0300

    Added test for happy path parsing

---
+++ pdp/yast_test.go
@@ -0,0 +1,98 @@
+package pdp
+
+import "testing"
+
+const YASTTestPolicy = `# Test policy
+attributes:
+  b: boolean
+  s: string
+  a: address
+  "n": network
+  d: domain
+  ss: Set of Strings
+  sn: Set of Networks
+  sd: Set of Domains
+
+policies:
+  id: root
+  policies:
+    - id: PolicySet #1
+      target:
+        - any:
+            - all:
+                - equal:
+                    - attr: s
+                    - val:
+                        type: string
+                        content: test
+                - contains:
+                    - val:
+                        type: network
+                        content: 127.0.0.0/8
+                    - attr: a
+
+            - contains:
+                - val:
+                    type: Set of Domains
+                    content: ["test.com", "example.com", "test.net", "example.net"]
+                - attr: d
+
+        - contains:
+            - val:
+               type: Set of Strings
+               content: ["test", "example"]
+            - attr: s
+
+      policies:
+        - id: Policy #1.1
+          target:
+            - equal:
+                - attr: s
+                - val:
+                    type: string
+                    content: test
+
+          rules:
+            - id: Rule #1.1.1
+              target:
+                - contains:
+                    - attr: d
+                    - val:
+                        type: Set of Domains
+                        content: ["test.com", "example.com", "test.net", "example.net"]
+
+              condition:
+                and:
+                  - not:
+                      - attr: b
+                  - or:
+                      - equal:
+                          - attr: s
+                          - val:
+                              type: string
+                              content: example
+                      - contains:
+                          - attr: s
+                          - val:
+                              type: string
+                              content: substring
+
+              obligations:
+                - d: example.org
+
+              effect: Permit
+
+          alg: FirstApplicableEffect
+
+  alg: FirstApplicableEffect`
+
+func TestUnmarshalYAST(t *testing.T) {
+	p, err := UnmarshalYAST([]byte(YASTTestPolicy), "", nil)
+	if err != nil {
+		t.Errorf("Expected no errors but got:\n%#v\n\n%s\n", err, err)
+	} else {
+		if p == nil {
+			t.Errorf("Expected policy but got nothing")
+		}
+	}
+}
