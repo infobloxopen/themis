248a905deb9a23cfa65a26e690342457090dea1e
Uladzimir Trehubenka <utrehubenka@infoblox.com>
2017-09-26 20:37:40 +0300

    Refactoring (#67)

--- contrib/coredns/policy/setup.go
+++ contrib/coredns/policy/setup.go
@@ -51,7 +51,7 @@ func setup(c *caddy.Controller) error {
 }
 
 func policyParse(c *caddy.Controller) (*PolicyPlugin, error) {
-	policyPlugin := &PolicyPlugin{}
+	policyPlugin := &PolicyPlugin{options: make(map[uint16]edns0Map)}
 
 	for c.Next() {
 		if c.Val() == "policy" {
