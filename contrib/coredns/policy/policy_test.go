2ae995a131d8d38a8c2ad7643a91d09fbe93e996
Uladzimir Trehubenka <utrehubenka@infoblox.com>
2017-09-27 17:05:44 +0300

    Improved UT for policy plugin

--- contrib/coredns/policy/policy_test.go
+++ contrib/coredns/policy/policy_test.go
@@ -1,8 +1,10 @@
 package policy
 
 import (
+	"encoding/hex"
 	"errors"
 	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/coredns/coredns/plugin"
@@ -39,6 +41,13 @@ func TestPolicy(t *testing.T) {
 			errResp:   errFakePdp,
 			status:    dns.RcodeServerFailure,
 			err:       errFakePdp,
+		},
+		{
+			query:     "test.com.",
+			queryType: dns.TypeA,
+			response:  &pdp.Response{Effect: pdp.Response_INDETERMINATE},
+			status:    dns.RcodeRefused,
+			err:       nil,
 		},
 		{
 			query:     "test.com.",
@@ -102,6 +111,15 @@ func TestPolicy(t *testing.T) {
 			response:  &pdp.Response{Effect: pdp.Response_PERMIT},
 			responseIP: &pdp.Response{Effect: pdp.Response_DENY,
 				Obligation: []*pdp.Attribute{{"redirect_to", "string", "redirect.biz"}}},
+			status: dns.RcodeSuccess,
+			err:    nil,
+		},
+		{
+			query:     "test.org.",
+			queryType: dns.TypeAAAA,
+			response:  &pdp.Response{Effect: pdp.Response_PERMIT},
+			responseIP: &pdp.Response{Effect: pdp.Response_DENY,
+				Obligation: []*pdp.Attribute{{"redirect_to", "string", "2001:db8:0:200:0:0:0:7"}}},
 			status: dns.RcodeSuccess,
 			err:    nil,
 		},
@@ -171,23 +189,11 @@ func TestPolicy(t *testing.T) {
 	rec := dnstest.NewRecorder(&test.ResponseWriter{})
 
 	for i, test := range tests {
+		// Make request
 		req := new(dns.Msg)
 		req.SetQuestion(test.query, test.queryType)
-		// add ENDS options to request
-		o := new(dns.OPT)
-		o.Hdr.Name = "."
-		o.Hdr.Rrtype = dns.TypeOPT
-		e := new(dns.EDNS0_LOCAL)
-		e.Code = 0xfffa
-		e.Data = []byte("4e7e318384088e7d4f3dbc96219ee5d4")
-		o.Option = append(o.Option, e)
-		req.Extra = append(req.Extra, o)
 		// Init test mock client
 		pm.pdp = newTestClientInit(test.response, test.responseIP, test.errResp, test.errRespIP)
-		// Add EDNS mapping
-		pm.AddEDNS0Map("0xfffa", "client_id", "hex", "string", "0", "32")
-		pm.AddEDNS0Map("0xfffb", "client_src", "address", "string", "", "")
-		pm.AddEDNS0Map("0xfffc", "client_mac", "bytes", "string", "", "")
 		// Handle request
 		status, err := pm.ServeDNS(context.TODO(), rec, req)
 		// Check status
@@ -202,6 +208,7 @@ func TestPolicy(t *testing.T) {
 
 	pm.DebugSuffix = "debug."
 	for i, test := range tests {
+		// Make request
 		req := new(dns.Msg)
 		req.Question = make([]dns.Question, 1)
 		req.Question[0] = dns.Question{test.query + pm.DebugSuffix, dns.TypeTXT, dns.ClassCHAOS}
@@ -240,7 +247,7 @@ func handler() plugin.Handler {
 			}
 		case "test.org.":
 			r.Answer = []dns.RR{
-				test.AAAA("test.org.	600	IN	AAAA		21DA:D3:0:2F3B:2AA:FF:FE28:9C5A"),
+				test.AAAA("test.org.	600	IN	AAAA		21da:d3:0:2f3b:2aa:ff:fe28:9c5a"),
 			}
 		case "redirect.biz.":
 			r.Answer = []dns.RR{
@@ -313,4 +320,112 @@ func fillResponse(in *pdp.Response, out interface{}) error {
 	r.Effect = in.Effect
 	r.Obligation = in.Obligation
 	return nil
+}
+
+func makeRequestWithEDNS0(code uint16, hexstring string, nonlocal bool) *dns.Msg {
+	req := new(dns.Msg)
+	o := new(dns.OPT)
+	o.Hdr.Name = "."
+	o.Hdr.Rrtype = dns.TypeOPT
+	if nonlocal {
+		e := new(dns.EDNS0_SUBNET)
+		o.Option = append(o.Option, e)
+	} else {
+		e := new(dns.EDNS0_LOCAL)
+		e.Code = code
+		src := []byte(hexstring)
+		dst := make([]byte, hex.DecodedLen(len(src)))
+		hex.Decode(dst, src)
+		e.Data = dst
+		o.Option = append(o.Option, e)
+	}
+	req.Extra = append(req.Extra, o)
+	return req
+}
+
+func TestEdns(t *testing.T) {
+	pm := PolicyPlugin{options: make(map[uint16]edns0Map)}
+
+	// Add EDNS mapping
+	if err := pm.AddEDNS0Map("0xfffa", "client_id", "hex", "string", "0", "32"); err != nil {
+		t.Errorf("Expected error 'nil' but got %v\n", err)
+	}
+	if err := pm.AddEDNS0Map("0xfffb", "source_ip", "address", "address", "0", "0"); err != nil {
+		t.Errorf("Expected error 'nil' but got %v\n", err)
+	}
+	if err := pm.AddEDNS0Map("0xfffc", "client_name", "bytes", "string", "0", "0"); err != nil {
+		t.Errorf("Expected error 'nil' but got %v\n", err)
+	}
+
+	tests := []struct {
+		name     string
+		code     uint16
+		data     string
+		ip       string
+		attr     map[string]*pdp.Attribute
+		nonlocal bool
+	}{
+		{
+			name: "Test different than EDNS0_LOCAL option",
+			ip:   "192.168.0.1",
+			attr: map[string]*pdp.Attribute{
+				"source_ip": {Id: "source_ip", Type: "address", Value: "192.168.0.1"},
+			},
+			nonlocal: true,
+		},
+		{
+			name: "Test option that not in config mapping",
+			code: 0xfffd,
+			data: "cafecafe",
+			ip:   "192.168.0.2",
+			attr: map[string]*pdp.Attribute{
+				"source_ip": {Id: "source_ip", Type: "address", Value: "192.168.0.2"},
+			},
+		},
+		{
+			name: "Test option handled as hex",
+			code: 0xfffa,
+			data: "4e7e318384088e7d4f3dbc96219ee5d4",
+			ip:   "192.168.0.3",
+			attr: map[string]*pdp.Attribute{
+				"source_ip": {Id: "source_ip", Type: "address", Value: "192.168.0.3"},
+				"client_id": {Id: "client_id", Type: "string", Value: "4e7e318384088e7d4f3dbc96219ee5d4"},
+			},
+		},
+		{
+			name: "Test option 'source_ip' handled as address",
+			code: 0xfffb,
+			data: "aca80001", // 172.168.0.1 in hex
+			ip:   "192.168.0.4",
+			attr: map[string]*pdp.Attribute{
+				"source_ip":       {Id: "source_ip", Type: "address", Value: "172.168.0.1"},
+				"proxy_source_ip": {Id: "proxy_source_ip", Type: "address", Value: "192.168.0.4"},
+			},
+		},
+		{
+			name: "Test option handled as bytes",
+			code: 0xfffc,
+			data: "637573746f6d6572", // "customer" in hex
+			ip:   "192.168.0.5",
+			attr: map[string]*pdp.Attribute{
+				"source_ip":   {Id: "source_ip", Type: "address", Value: "192.168.0.5"},
+				"client_name": {Id: "client_name", Type: "string", Value: "customer"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		req := makeRequestWithEDNS0(test.code, test.data, test.nonlocal)
+		attr := pm.getAttrsFromEDNS0(req, test.ip)
+		mapAttr := make(map[string]*pdp.Attribute)
+		for _, a := range attr {
+			mapAttr[a.Id] = a
+		}
+		if len(attr) != len(mapAttr) {
+			t.Errorf("%s: array %q transformed to map %q\n", test.name, attr, mapAttr)
+		}
+		if !reflect.DeepEqual(test.attr, mapAttr) {
+			t.Errorf("%s: expected attributes %q but got %q\n", test.name, test.attr, mapAttr)
+		}
+	}
 }
