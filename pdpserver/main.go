01036452b32377e04e8a2acde9d70d9344cf3d43
Sergey Kudriavtsev <skudriavtsev@infoblox.com>
2017-10-10 17:28:36 +0300

    trivial optimization

--- pdpserver/main.go
+++ pdpserver/main.go
@@ -22,4 +22,8 @@ func main() {
 	runtime.GC()
 
 	pdp.serve()
+
+	// Prevent the main Go-routine to be finished,
+	// while other Go-routines are serving requests.
+	select {}
 }
--- pdpserver/server.go
+++ pdpserver/server.go
@@ -241,7 +241,4 @@ func (s *server) serve() {
 		// serveRequests() will be executed by external request.
 		log.Info("Waiting for policies to be applied.")
 	}
-	for {
-		time.Sleep(time.Second)
-	}
 }
