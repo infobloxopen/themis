c64b8aac07c4e4f1cab3153ff00a50d294177b20
Vasili Vasilyeu <vasili.v@tut.by>
2017-10-13 12:59:13 +0300

    Added streaming support to pepcli

--- pepcli/perf/perf.go
+++ pepcli/perf/perf.go
@@ -12,7 +12,7 @@ const (
 	Description = "measures performance of evaluation given requests on PDP server"
 )
 
-func Exec(addr, in, out string, n int, v interface{}) error {
+func Exec(addr, in, out string, n, s int, v interface{}) error {
 	reqs, err := requests.Load(in)
 	if err != nil {
 		return fmt.Errorf("can't load requests from \"%s\"", in)
@@ -22,14 +22,26 @@ func Exec(addr, in, out string, n int, v interface{}) error {
 		n = len(reqs)
 	}
 
-	c := pep.NewClient()
+	opts := []pep.Option{}
+	if s > 0 {
+		opts = append(opts,
+			pep.WithStreams(s),
+		)
+	}
+
+	c := pep.NewClient(opts...)
 	err = c.Connect(addr)
 	if err != nil {
 		return fmt.Errorf("can't connect to %s: %s", addr, err)
 	}
 	defer c.Close()
 
-	recs, err := measurement(c, n, v.(config).parallel, v.(config).limit, reqs)
+	validate := c.ModalValidate
+	if s > 0 {
+		validate = c.StreamValidate
+	}
+
+	recs, err := measurement(validate, n, v.(config).parallel, v.(config).limit, reqs)
 	if err != nil {
 		return err
 	}
