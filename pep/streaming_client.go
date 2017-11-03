43771d72e1d64580024626db7cbd5b84716bffbd
Vasili Vasilyeu <vasili.v@tut.by>
2017-11-03 16:30:22 +0300

    Added correct all streams termination with timeout

--- pep/streaming_client.go
+++ pep/streaming_client.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
 	ot "github.com/opentracing/opentracing-go"
@@ -22,7 +23,8 @@ type pdpStreamingClient struct {
 	opts options
 
 	lock    *sync.RWMutex
-	streams chan pb.PDP_NewValidationStreamClient
+	streams []pb.PDP_NewValidationStreamClient
+	index   chan int
 }
 
 func (c *pdpStreamingClient) Connect(addr string) error {
@@ -68,10 +70,12 @@ func (c *pdpStreamingClient) Connect(addr string) error {
 }
 
 func (c *pdpStreamingClient) Close() {
+	ready := c.waitForStreams()
+
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	c.closeStreams()
+	c.closeStreams(ready)
 
 	if c.conn != nil {
 		c.conn.Close()
@@ -91,6 +95,21 @@ func (c *pdpStreamingClient) Validate(in, out interface{}) error {
 	return s.validate(in, out)
 }
 
+func (c *pdpStreamingClient) fillIndex() {
+	c.index = make(chan int, len(c.streams))
+	for i := range c.streams {
+		c.index <- i
+	}
+}
+
+func (c *pdpStreamingClient) dropIndex() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	close(c.index)
+	c.index = nil
+}
+
 func (c *pdpStreamingClient) makeStreams() error {
 	if c.opts.maxStreams <= 0 {
 		panic(fmt.Errorf("streaming client must be created with at least 1 stream but got %d", c.opts.maxStreams))
@@ -107,65 +126,119 @@ func (c *pdpStreamingClient) makeStreams() error {
 		return nil
 	}
 
-	c.streams = make(chan pb.PDP_NewValidationStreamClient, c.opts.maxStreams)
-	for i := 0; i < c.opts.maxStreams; i++ {
+	c.streams = make([]pb.PDP_NewValidationStreamClient, c.opts.maxStreams)
+	ready := make([]int, len(c.streams))
+	for i := range ready {
+		ready[i] = -1
+	}
+
+	for i := range c.streams {
 		s, err := (*c.client).NewValidationStream(context.TODO(),
 			grpc.FailFast(false),
 		)
 		if err != nil {
-			c.closeStreams()
+			c.closeStreams(ready)
 			return err
 		}
 
-		c.streams <- s
+		c.streams[i] = s
+		ready[i] = i
 	}
+
+	c.fillIndex()
 
 	return nil
 }
 
-func (c *pdpStreamingClient) closeStreams() {
-	if c.streams == nil {
+func (c *pdpStreamingClient) waitForStreams() []int {
+	c.lock.RLock()
+	streams := c.streams
+	index := c.index
+	c.lock.RUnlock()
+
+	if index == nil {
+		return nil
+	}
+
+	ready := make([]int, len(streams))
+	for i := range ready {
+		ready[i] = -1
+	}
+
+	timeout := time.After(5 * time.Second)
+	for i := range ready {
+		select {
+		case idx := <-index:
+			ready[i] = idx
+
+		case <-timeout:
+			c.dropIndex()
+			return ready
+		}
+	}
+
+	c.dropIndex()
+	return ready
+}
+
+func (c *pdpStreamingClient) closeStreams(ready []int) {
+	if len(c.streams) <= 0 {
 		return
 	}
 
-	var wg sync.WaitGroup
-	for len(c.streams) > 0 {
-		s := <-c.streams
-		if err := s.CloseSend(); err != nil {
-			continue
+	wg := &sync.WaitGroup{}
+	for _, i := range ready {
+		if i >= 0 {
+			wg.Add(1)
+			go closeStream(c.streams[i], wg)
 		}
-
-		wg.Add(1)
-		go func(s pb.PDP_NewValidationStreamClient) {
-			defer wg.Done()
-
-			var msg interface{}
-			s.RecvMsg(&msg)
-		}(s)
 	}
 	wg.Wait()
-
-	close(c.streams)
 	c.streams = nil
+}
+
+func closeStream(s pb.PDP_NewValidationStreamClient, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	if err := s.CloseSend(); err != nil {
+		return
+	}
+
+	done := make(chan int)
+	go func() {
+		defer close(done)
+
+		var msg pb.Response
+		s.RecvMsg(&msg)
+	}()
+
+	select {
+	case <-done:
+	case <-time.After(5 * time.Second):
+	}
 }
 
 func (c *pdpStreamingClient) getStream() (stream, error) {
 	c.lock.RLock()
-	ch := c.streams
+	streams := c.streams
+	index := c.index
 	c.lock.RUnlock()
 
-	if ch == nil {
+	if streams == nil {
 		if err := c.makeStreams(); err != nil {
 			return stream{}, err
 		}
 
 		c.lock.RLock()
-		ch = c.streams
+		streams = c.streams
+		index = c.index
 		c.lock.RUnlock()
 	}
 
-	if s, ok := <-ch; ok {
-		return stream{s: s, ch: ch}, nil
+	if index != nil {
+		if i, ok := <-index; ok {
+			return stream{idx: i, s: streams[i], ch: index}, nil
+		}
 	}
 
 	return stream{}, ErrorNotConnected
@@ -175,17 +248,18 @@ func (c *pdpStreamingClient) putStream(s stream) error {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
 
-	if c.streams == nil || c.streams != s.ch {
+	if c.index == nil || c.index != s.ch {
 		return errors.New("invalid connection")
 	}
 
-	s.ch <- s.s
+	s.ch <- s.idx
 	return nil
 }
 
 type stream struct {
-	s  pb.PDP_NewValidationStreamClient
-	ch chan pb.PDP_NewValidationStreamClient
+	idx int
+	s   pb.PDP_NewValidationStreamClient
+	ch  chan int
 }
 
 func (s stream) validate(in, out interface{}) error {
