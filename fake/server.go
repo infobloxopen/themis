8ad7e63cecf1043fa077f28455d0a16bb2d6dcd7
Tommy Ong <tong@infoblox.com>
2019-11-21 17:26:38 -0800

    add fake

---
+++ fake/server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+  "context"
+  "log"
+  "net"
+
+  "google.golang.org/grpc"
+  "google.golang.org/grpc/codes"
+  "google.golang.org/grpc/status"
+
+  pb "github.com/infobloxopen/themis/pdp-service"
+)
+
+const (
+  port = ":5555"
+)
+
+type Server struct {}
+
+func (s *Server) Validate(ctx context.Context, msg *pb.Msg) (*pb.Msg, error) {
+  resp := &pdp.Response{}
+
+  resp.Effect = pdp.EffectPermit
+  resp.Obligations = []pdp.AttributeAssignment{}
+  resp.Status = nil
+
+  body, _ := resp.Marshal(nil)
+
+  return &pb.Msg{Body: body}, nil
+}
+
+func (s *Server) NewValidationStream(srv pb.PDP_NewValidationStreamServer) error {
+  return status.Errorf(codes.Unimplemented, "NewValidationStream is not supported")
+}
+
+func main(){
+  lis, err := net.Listen("tcp", port)
+  if err != nil {
+    log.Fatalf("failed to listen: %v", err)
+  }
+
+  s := grpc.NewServer()
+  pb.RegisterPDPServer(s, &Server{})
+
+  if err := s.Serve(lis); err != nil {
+    log.Fatal("failed to serve: %v", err)
+  }
+}
